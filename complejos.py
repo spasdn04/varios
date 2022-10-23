eda98db12282f8b0904c1dea832f1d7e33a57428
spas <spas2004dn@gmail.com>
2022-10-23 22:27:42 +0200

    23-10-2022

---
+++ complejos.py
@@ -0,0 +1,27 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+z = complex(2, 3)
+z2 = complex(2, -3)
+z3 = (z + z2) / 2
+z4 = (z - z2) / 2
+
+print(z, '\n', z2, '\n', z3, '\n', z4)
+
+fig, ax = plt.subplots()
+
+theta = np.linspace(0, 2*np.pi, 100)
+
+radius = 1
+
+a = radius*np.cos(theta)
+b = radius*np.sin(theta)
+x = [.9, 0, 0]
+y = [.7, 1, 0]
+
+ax.plot(a, b, x, y, 'ro', linewidth=1.5, color='red')
+ax.set_aspect(1)
+
+plt.show()
